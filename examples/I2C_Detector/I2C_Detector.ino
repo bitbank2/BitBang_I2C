43ee7ffb6fca6ca755736c187b0417e5a64903ba
Verkehrsrot <cyberman@obh.snafu.de>
2020-10-14 10:49:34 +0200

    Update I2C_Detector.ino

--- examples/I2C_Detector/I2C_Detector.ino
+++ examples/I2C_Detector/I2C_Detector.ino
@@ -55,7 +55,7 @@
 const char *szNames[]  = {"Unknown","SSD1306","SH1106","VL53L0X","BMP180", "BMP280","BME280",
                 "MPU-60x0", "MPU-9250", "MCP9808","LSM6DS3", "ADXL345", "ADS1115","MAX44009",
                 "MAG3110", "CCS811", "HTS221", "LPS25H", "LSM9DS1","LM8330", "DS3231", "LIS3DH",
-                "LIS3DSH","INA219","SHT3X","HDC1080","MPU6886","BME680"};
+                "LIS3DSH","INA219","SHT3X","HDC1080","MPU6886","BME680", "AXP202", "AXP192"};
 #endif
 
 BBI2C bbi2c;
